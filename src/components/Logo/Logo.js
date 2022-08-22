c1a5afbbbd4cd0cd23753ee057da2d1d63d3e748
erampion <ch7128@gmail.com>
2022-08-22 01:26:15 -0400

    initial commit

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,22 @@
+import React from "react";
+// import ReactDOM from 'react-dom';
+// import Tilt from 'react-parallax-tilt';
+
+// const Logo = () => {
+//   return (
+//     <Tilt className="mw2">
+//       <div 
+//         style = {
+//           { width: '200px', 
+//             height: '200px', 
+//             backgroundColor: 'darkgreen',
+//             }
+//         }>
+//         <h1>React Parallax Tilt 👀</h1>
+//       </div>
+//     </Tilt>
+//   );
+// };
+
+
+export default Logo;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <nav className="pa3 fr">
+      <p className="ba br2 f3 link dim black pa2 pointer">Sing Out</p>
+    </nav>
+  )
+}
+
+export default Navigation
